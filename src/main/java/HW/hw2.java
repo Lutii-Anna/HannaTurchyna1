ac566ba34cea4903bddd7eeb36b20da9f3b70147
AnnaTurchyna <annalutisha@gmail.com>
2021-10-26 21:06:56 +0300

    домашня робота

--- src/main/java/HW/hw2.java
+++ src/main/java/HW/hw2.java
@@ -1,4 +1,11 @@
 package HW;
-
+import java.util.Scanner; // імпорт сканера
 public class hw2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("The height building 'height' m and 'count' floors : ");
+        String height = sc.nextLine();
+        String count = sc.nextLine();
+        System.out.println("The height building " + height + "m and " + count + " floors");
+    }
 }

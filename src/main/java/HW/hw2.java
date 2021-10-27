bcb7d06e5d049198a2ea874f4f14609f5168ae74
AnnaTurchyna <annalutisha@gmail.com>
2021-10-27 10:35:11 +0300

    домашня робота

--- src/main/java/HW/hw2.java
+++ src/main/java/HW/hw2.java
@@ -1,12 +1,11 @@
 package HW;
 import java.util.Scanner; // імпорт сканера
 public class hw2 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("The height building 'height' m and 'count' floors : ");
-        String height = sc.nextLine();
-        String count = sc.nextLine();
+        System.out.println("Enter the height of the building then the number of floors : ");
+        double height = sc.nextDouble();
+        int count = sc.nextInt();
         System.out.println("The height building " + height + "m and " + count + " floors");
     }
 }
